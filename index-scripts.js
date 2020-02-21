5a92faf0a9730cc8fd92135856de336fde5b3926
Marcio Rosa <marciodrosa@gmail.com>
2020-02-21 13:07:48 -0300

    Bring jquery js file to the project.

--- index-scripts.js
+++ index-scripts.js
@@ -78,6 +78,9 @@ $(window).on('load', function ()
 
 $(function() {
 	$("#squadron51").click(function() {
-		window.open("squadron51.html", "_self")
+		window.open("squadron51.html", "_self");
+	});
+	$("#squadron51").mouseenter(function() {
+		$("#squadron51").animate({transform: "scale(0.7)"});
 	});
 });
