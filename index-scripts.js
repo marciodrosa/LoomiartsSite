2a0ce52ecc1b8730f5fe94f114ec09a0b28946c4
Márcio Rosa <marciodrosa@gmail.com>
2020-02-24 00:41:40 -0300

    Added a rectangle with a text when hover the Squadron 51 banner.

--- index-scripts.js
+++ index-scripts.js
@@ -1,3 +1,12 @@
 $(function() {
-
+	$(".squadron51-container").hover(
+		function() {
+			$(".squadron51-container-content").fadeIn("slow")
+			$(".squadron51-container-rectangle").animate( { width: "300px" }, "fast");
+		},
+		function() {
+			$(".squadron51-container-content").fadeOut("fast")
+			$(".squadron51-container-rectangle").animate( { width: "0px" });
+		}
+	);
 });
