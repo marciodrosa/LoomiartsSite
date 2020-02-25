e87fc55ae969386e4e992d2a8b80ed36af1c5862
Márcio Rosa <marciodrosa@gmail.com>
2020-02-25 16:29:33 -0300

    Presskit adjustments.

--- presskit-i18n.js
+++ presskit-i18n.js
@@ -3,10 +3,12 @@ $(function() {
 	{
 		return {
 			strings: {
-				pressKitTitle:"PRESS KIT ESQUADRÃO 51",
+				pressKitTitle:"PRESS KIT",
+				imagesDownloadInfoLabel:"(Para ver as imagens em alta resolução, baixe ou abra em uma nova guia)",
+				pressKitLoomiartsTitle:"LOOMIARTS",
+				pressKitSquadron51Title:"ESQUADRÃO 51",
 				internationalTitleLabel:"Título internacional",
 				originalTitleLabel:"Título original",
-				publisherAndDeveloperLabel:"Publisher e desenvolvedora",
 				publisherAndDeveloperInfoText:"São Leopoldo, RS, Brasil\nloomiarts.com\nfacebook/loomiarts\ntwitter/loomiarts\ncontato@loomiarts.com",
 				synopsisLabel:"Sinopse",
 				synopsisText:"Há algumas décadas, a Terra foi descoberta por alienígenas. Após o primeiro contato com os seres humanos, os extraterrestres logo se instalaram por todo o planeta, trazendo muitas promessas de progresso e modernidade. No entanto, a relação logo se mostrou opressora e violenta. Surge então o grupo antiextraterrestre Esquadrão 51, que combate a opressão alienígena em batalhas aéreas pelos céus de todo o mundo.",
@@ -24,7 +26,6 @@ $(function() {
 				awardsText1:"BIG Festival 2017: Melhor Arte (finalista), Melhor Jogo Brasileiro (finalista)",
 				awardsText2:"Festival de Jogos do SBGames 2017: Melhor Visual (vencedor), Melhor Áudio (finalista), Melhor Tecnologia (finalista)",
 				inGameScreenshotsLabel:"Screenshots do jogo",
-				inGameScreenshotsInfoLabel:"(Botão direito do mouse > \"Salvar imagem como...\" para baixar a imagem em alta resolução)",
 				logosAndPromotionalImagesLabel:"Logos e imagens promocionais",
 				fmvProductionStillsLabel:"Stills das filmagens das sequências em FMV",
 			},
@@ -37,10 +38,12 @@ $(function() {
 	{
 		return {
 			strings: {
-				pressKitTitle:"SQUADRON 51 PRESS KIT",
+				pressKitTitle:"PRESS KIT",
+				imagesDownloadInfoLabel:"(To see the images at high resolution, save it or open it in a new tab)",
+				pressKitLoomiartsTitle:"LOOMIARTS",
+				pressKitSquadron51Title:"SQUADRON 51",
 				internationalTitleLabel:"International title",
 				originalTitleLabel:"Original title (portuguese)",
-				publisherAndDeveloperLabel:"Publisher and developer",
 				publisherAndDeveloperInfoText:"São Leopoldo, RS, Brazil\nloomiarts.com\nfacebook/loomiarts\ntwitter/loomiarts\ncontact@loomiarts.com",
 				synopsisLabel:"Synopsis",
 				synopsisText:"Decades ago, Earth was discovered by aliens. After the first encounter with the human beings, the extraterrestrials soon arrived by all over the planet, bring with them a lot of promises of progress and modernity. However, the relationship soon became oppressor and violent. Then, the antiextraterrestrial group Squadron 51 arrives, fighting the alien oppresion in aircraft battles all around the world.",
@@ -58,7 +61,6 @@ $(function() {
 				awardsText1:"BIG Festival 2017: Best Arte (finalist), Best Brazilian Game (finalist)",
 				awardsText2:"SBGames 2017: Best Graphics (winner), Best Sound (finalist), Best Technology (finalist)",
 				inGameScreenshotsLabel:"In-game screenshots",
-				inGameScreenshotsInfoLabel:"(Right click > \"Save image as...\" to download high resolution images)",
 				logosAndPromotionalImagesLabel:"Logos and promotional images",
 				fmvProductionStillsLabel:"FMV production stills",
 			},
