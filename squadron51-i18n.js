39d5d8deb5b3caa8df97d48b5dba83267db8f0cf
Márcio Rosa <marciodrosa@gmail.com>
2020-02-26 16:33:47 -0300

    New i18n strings for the Squadron 51 page (not yet translated to english).

--- squadron51-i18n.js
+++ squadron51-i18n.js
@@ -3,20 +3,23 @@ $(function() {
 	{
 		return {
 			strings: {
-				squadron51HeadlineText:"ALIENÍGENAS DESCOBREM A TERRA!",
-				squadron51Text1:"Junte-se ao ESQUADRÃO 51 e lute contra os inimigos extraterrestres!",
-				squadron51Text2:"ESQUADRÃO 51 é um SHMUP que coloca o jogador na pele da Tenente Kaya em batalhas nos céus de todo o planeta! Jogue como se estivesse em uma autêntica ficção científica dos anos 50!",
-				squadron51MainFeaturesHeadlineText:"PRINCIPAIS CARACTERÍSTICAS",
-				squadron51FeatureText1:"• Seis fases, diferentes tipos de ambientes: sobre o oceano, montanhas geladas, cavernas, tempestades e muitos outros",
-				squadron51FeatureText2:"• Quatro diferentes aeronaves pilotadas ao longo do jogo",
-				squadron51FeatureText3:"• Narrativa contada através de cutscenes (filmadas em live-action, como um filme!) e diálogos entre os pilotos, que podem ser ouvidos durante o gameplay",
-				squadron51FeatureText4:"• Multiplayer local cooperativo para dois jogadores",
+				firstStripTitle: "ELES DESCOBRIRAM A TERRA",
+				firstStripText1: "Extraterrestres, liderados pelo temível DIRETOR ZAROG, chegaram ao nosso planeta com promessas de um novo e promissor futuro para os terráqueos. No entanto, o relacionamento logo se tornou tenso com o crescimento desenfreado das operações da central empreendedora alienígena na Terra, a CORPORAÇÃO VEGA, que logo passou a utilizar o seu grande poder de influência entre os governantes para conquistar forças de trabalho humanas cada vez mais baratas e abusivas.",
+				firstStripText2: "Em meio à situações opressoras cada vez maiores e mais evidentes, o grupo rebelde ESQUADRÃO 51 surge como a resposta dos oprimidos perante aos abusos e violência perpetuados pelos extraterrestres. Agora, resta saber: estariam os ávidos pilotos do Esquadrão e seus aviões à altura da maquiavélica frota de discos voadores da Corporação alienígena?",
+				cardGameplayTitle: "Gameplay clássico",
+				cardGameplayText: "ESQUADRÃO 51 é um jogo no estilo SHMUP com quatro tipo de aviões para pilotar e até dois armamentos especiais para equipar",
+				cardLevelsTitle: "11 fases",
+				cardLevelsText: "Batalhe contra as naves inimigas e chefões em ambientes diversos - como neve, floresta, cidade e muitos outros",
+				cardMovieTitle: "Experiência cinematográfica",
+				cardMovieText: "Visual em GLORIOSO preto e branco, sequências em FMV e dublagem contam a história da batalha entre o Esquadrão 51 contra a opressão do Diretor Zarog",
+				cardMultiplayerTitle: "Multiplayer",
+				cardMultiplayerText: "Inclui modo para um ou dois jogadores - cooperativo e local. Conquiste pontos com o seu amigo e desbloqueiem juntos novos armamentos especiais e upgrades",
+				trailerStripText: "Junte-se à causa rebelde! No papel da TENENTE KAYA, enfrente discos voadores, caças extraterrestres, monstros e naves futurísticas diversas em uma batalha diretamente dos anos 50! Lembre-se: o futuro da Terra está em suas mãos!",
+				galleryTitle: "Galeria",
+				coproductionText: "Esquadrão 51 é uma coprodução entre",
+				comingSoon: "Em breve para",
 			},
 			images: {
-				squadron51BannerImage: {
-					src:"Images/BannerSitePT.jpg",
-					alt:"Logo do jogo Esquadrão 51"
-				}
 			}
 		};
 	}
@@ -25,20 +28,23 @@ $(function() {
 	{
 		return {
 			strings: {
-				squadron51HeadlineText:"THE ALIENS DISCOVER EARTH!",
-				squadron51Text1:"Join the SQUADRON 51 and fight against extraterrestrial enemies!",
-				squadron51Text2:"SQUADRON 51 is a SHMUP game where the player plays as Lieutenant Kaya in aircraft battles all around the world! Play as you were in an authentic sci-fi movie from the fifties!",
-				squadron51MainFeaturesHeadlineText:"MAIN FEATURES",
-				squadron51FeatureText1:"• Six levels, many kinds of environments: above the ocean, glacial montains, caves, storms and many others",
-				squadron51FeatureText2:"• Four different aircrafts to play along the game",
-				squadron51FeatureText3:"• Story that is told through cutscenes (FMV sequences, like a movie!) and dialogues between the pilots during gameplay",
-				squadron51FeatureText4:"• Local and cooperative multiplayer mode for two players",
+				firstStripTitle: "ELES DESCOBRIRAM A TERRA",
+				firstStripText1: "Extraterrestres, liderados pelo temível DIRETOR ZAROG, chegaram ao nosso planeta com promessas de um novo e promissor futuro para os terráqueos. No entanto, o relacionamento logo se tornou tenso com o crescimento desenfreado das operações da central empreendedora alienígena na Terra, a CORPORAÇÃO VEGA, que logo passou a utilizar o seu grande poder de influência entre os governantes para conquistar forças de trabalho humanas cada vez mais baratas e abusivas.",
+				firstStripText2: "Em meio à situações opressoras cada vez maiores e mais evidentes, o grupo rebelde ESQUADRÃO 51 surge como a resposta dos oprimidos perante aos abusos e violência perpetuados pelos extraterrestres. Agora, resta saber: estariam os ávidos pilotos do Esquadrão e seus aviões à altura da maquiavélica frota de discos voadores da Corporação alienígena?",
+				cardGameplayTitle: "Gameplay clássico",
+				cardGameplayText: "ESQUADRÃO 51 é um jogo no estilo SHMUP com quatro tipo de aviões para pilotar e até dois armamentos especiais para equipar",
+				cardLevelsTitle: "11 fases",
+				cardLevelsText: "Batalhe contra as naves inimigas e chefões em ambientes diversos - como neve, floresta, cidade e muitos outros",
+				cardMovieTitle: "Experiência cinematográfica",
+				cardMovieText: "Visual em GLORIOSO preto e branco, sequências em FMV e dublagem contam a história da batalha entre o Esquadrão 51 contra a opressão do Diretor Zarog",
+				cardMultiplayerTitle: "Multiplayer",
+				cardMultiplayerText: "Inclui modo para um ou dois jogadores - cooperativo e local. Conquiste pontos com o seu amigo e desbloqueiem juntos novos armamentos especiais e upgrades",
+				trailerStripText: "Junte-se à causa rebelde! No papel da TENENTE KAYA, enfrente discos voadores, caças extraterrestres, monstros e naves futurísticas diversas em uma batalha diretamente dos anos 50! Lembre-se: o futuro da Terra está em suas mãos!",
+				galleryTitle: "Galeria",
+				coproductionText: "Esquadrão 51 é uma coprodução entre",
+				comingSoon: "Em breve para",
 			},
 			images: {
-				squadron51BannerImage: {
-					src:"Images/BannerSiteEN.jpg",
-					alt:"Squadron 51 game logo"
-				}
 			}
 		};
 	}
