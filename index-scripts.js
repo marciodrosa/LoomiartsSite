a83408c17a372ef71bc17b82592c581cc1907210
Márcio Rosa <marciodrosa@gmail.com>
2022-01-23 19:10:48 -0300

    Refactor to be easier to add more banners for future games.

--- index-scripts.js
+++ index-scripts.js
@@ -2,46 +2,36 @@ $(function() {
 	createNavMenu("navbar", {loomiartsLogo: "#", home: "#home", contact: "#contact"});
 	createFooter("footer");
 
-	var showingSquadron51Info = false;
-
-	var showSquadron51Menu = function() {
-		$("#squadron51-container .game-menu-content").stop().fadeIn({duration: "slow", queue: true});
-		$("#squadron51-container .game-menu-rectangle").stop().animate({height: "100%"}, { duration: "fast", queue: true});
+	var showGameMenu = function() {
+		$(this).parents(".game-container").find(".game-menu-content").stop().fadeIn({duration: "slow", queue: true});
+		$(this).parents(".game-container").find(".game-menu-rectangle").stop().animate({height: "100%"}, { duration: "fast", queue: true});
 	}
 
-	var hideSquadron51Menu = function() {
-		if (!showingSquadron51Info) {
-			$("#squadron51-container .game-menu-content").stop().fadeOut({duration: "fast", queue: true});
-			$("#squadron51-container .game-menu-rectangle").stop().animate({height: "0%"}, { queue: true});
+	var hideGameMenu = function() {
+		if ($(this).parents(".game-container").find(".game-menu-item-info").attr("data-active") != "true") {
+			$(this).parents(".game-container").find(".game-menu-content").stop().fadeOut({duration: "fast", queue: true});
+			$(this).parents(".game-container").find(".game-menu-rectangle").stop().animate({height: "0%"}, { queue: true});
 		}
 	}
 
-	var showSquadron51Info = function() {
-		if (!showingSquadron51Info) {
-			showingSquadron51Info = true;
-			showSquadron51Menu()
-			$("#squadron51-container .game-info-content").stop().fadeIn({duration: "fast", queue: true});
-			$("#squadron51-info-button").attr("data-active", true);
+	var showGameInfo = function() {
+		if ($(this).parents(".game-container").find(".game-menu-item-info").attr("data-active") != "true") {
+			showGameMenu.call(this);
+			toggleGameInfo.call(this);
 		}
 	}
 
-	var hideSquadron51Info = function() {
-		if (showingSquadron51Info) {
-			showingSquadron51Info = false;
-			$("#squadron51-container .game-info-content").stop().fadeOut({duration: "fast", queue: true});
-			$("#squadron51-info-button").attr("data-active", false);
-		}
-	}
-
-	var toggleSquadron51Info = function() {
-		if (showingSquadron51Info) {
-			hideSquadron51Info()
+	var toggleGameInfo = function() {
+		if ($(this).attr("data-active") == "true") {
+			$(this).parents(".game-container").find(".game-info-content").fadeOut({duration: "fast", queue: true});
+			$(this).parents(".game-container").find(".game-menu-item-info").attr("data-active", "false");
 		} else {
-			showSquadron51Info()
+			$(this).parents(".game-container").find(".game-info-content").fadeIn({duration: "fast", queue: true});
+			$(this).parents(".game-container").find(".game-menu-item-info").attr("data-active", "true");
 		}
 	}
 
-	$("#squadron51-container .game-banner-container").hover(showSquadron51Menu, hideSquadron51Menu);
-	$("#squadron51-container .game-banner-container > img").click(showSquadron51Info);
-	$("#squadron51-info-button").click(toggleSquadron51Info)
+	$(".game-banner-container").hover(showGameMenu, hideGameMenu);
+	$(".game-banner-container > img").click(showGameInfo);
+	$(".game-menu-item-info").click(toggleGameInfo)
 });
