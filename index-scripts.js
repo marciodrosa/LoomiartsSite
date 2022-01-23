0d441f167b2eed4fb3bb24ff3aa3e742eb82a6b1
Márcio Rosa <marciodrosa@gmail.com>
2022-01-23 16:07:33 -0300

    WIP of a way more simplified site with all links related to Squadron 51 in the index page instead of a dedicated page.

--- index-scripts.js
+++ index-scripts.js
@@ -1,14 +1,45 @@
 $(function() {
 	createNavMenu("navbar", {loomiartsLogo: "#", home: "#home", contact: "#contact"});
 	createFooter("footer");
-	$(".squadron51-container").hover(
-		function() {
-			$(".squadron51-container-content").stop().fadeIn({duration: "slow", queue: true});
-			$(".squadron51-container-rectangle").stop().animate({width: "300px"}, { duration: "fast", queue: true});
-		},
-		function() {
-			$(".squadron51-container-content").stop().fadeOut({duration: "fast", queue: true});
-			$(".squadron51-container-rectangle").stop().animate({width: "0px"}, { queue: true});
+
+	var showingSquadron51Info = false;
+
+	var showSquadron51Menu = function() {
+		$("#squadron51-container .game-menu-content").stop().fadeIn({duration: "slow", queue: true});
+		$("#squadron51-container .game-menu-rectangle").stop().animate({height: "100%"}, { duration: "fast", queue: true});
+	}
+
+	var hideSquadron51Menu = function() {
+		if (!showingSquadron51Info) {
+			$("#squadron51-container .game-menu-content").stop().fadeOut({duration: "fast", queue: true});
+			$("#squadron51-container .game-menu-rectangle").stop().animate({height: "0%"}, { queue: true});
 		}
-	);
+	}
+
+	var showSquadron51Info = function() {
+		if (!showingSquadron51Info) {
+			showingSquadron51Info = true;
+			showSquadron51Menu()
+			$("#squadron51-container .game-info-content").stop().fadeIn({duration: "fast", queue: true});
+		}
+	}
+
+	var hideSquadron51Info = function() {
+		if (showingSquadron51Info) {
+			showingSquadron51Info = false;
+			$("#squadron51-container .game-info-content").stop().fadeOut({duration: "fast", queue: true});
+		}
+	}
+
+	var toggleSquadron51Info = function() {
+		if (showingSquadron51Info) {
+			hideSquadron51Info()
+		} else {
+			showSquadron51Info()
+		}
+	}
+
+	$("#squadron51-container .game-banner-container").hover(showSquadron51Menu, hideSquadron51Menu);
+	$("#squadron51-container .game-banner-container > img").click(showSquadron51Info);
+	$("#squadron51-info-button").click(toggleSquadron51Info)
 });
